936b2c5b4470d608de3dcd039cb3c84eb68b5488
MikeGPQ <miguelgarciaparra@hotmail.com>
2025-07-08 13:54:03 -0600

    08/07/2025

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
-import { updateData } from "../firestore";
+import { deleteData, updateData } from "../firestore";
 
-export function Card({id, image, name, mail, puesto }) {
+export function Card({ id, image, name, mail, puesto }) {
+    const [editable, setEditable] = useState(false);
+
+    function toggleEdit() {
+        setEditable(!editable);
+    }
+
     const [user, setUser] = useState({
         nombre: name,
         correo: mail,
@@ -10,16 +16,27 @@ export function Card({id, image, name, mail, puesto }) {
         active: true
     });
 
-
-    async function handleSubmit(e) {
+    async function updateHandle(e) {
         e.preventDefault();
         try {
-            await updateData("empleados", {
-                ...user,
-                active: false
-            }, id);  
+            await updateData("empleados", user, id);  
+            setEditable(false);
         } catch (error) {
             console.error("Error updating user:", error);
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser(prev => ({ ...prev, [name]: value }));
+    }
+
+    async function deleteHandle(e) {
+        e.preventDefault();
+        try {
+            await deleteData("empleados",user, id);
+        } catch (error) {
+            console.error("Error deleting user:", error);
         }
     }
 
@@ -27,36 +44,98 @@ export function Card({id, image, name, mail, puesto }) {
         <div style={{ width: '18rem', borderRadius: '0.25rem', backgroundColor: '#4CAF50' }}>
             <div style={{ padding: '1.25rem', textAlign: 'center' }}>
 
-                <p style={{ color: '#FFFFFF', marginBottom: '1rem' }}>
+                
+
+                <button
+                    onClick={deleteHandle}
+                    style={{
+                        display: "flex", justifyContent: "center", alignItems: "center", width: '24px', height: '24px', padding: '0', backgroundColor: '#000000', color: "#FFFFFF", border: 'none', borderRadius: '50%', fontSize: '0.9rem', cursor: 'pointer', margin: 0,
+                        ':hover': {
+                            backgroundColor: '#388E3C'
+                        }
+                    }}
+                >
+                    x
+                </button>
+
+                <p style={{ margin: 0, color: '#FFFFFF', marginBottom: '1rem' }}>
                     {id}
                 </p>
 
                 <img src={image || "https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"} style={{ borderRadius: '50%', width: '150px', height: '150px', objectFit: 'cover', marginBottom: '1rem' }} />
+
+                {!editable && (
+                    <>
+                        {name && (
+                            <h3 style={{ marginBottom: '0.5rem', fontSize: '1.25rem', fontWeight: '500', lineHeight: '1.2', color: "#FFFFFF" }}>
+                                {name}
+                            </h3>
+                        )}
+
+                        {mail && (
+                            <p style={{ color: '#FFFFFF', marginBottom: '1rem' }}>
+                                Mail: {mail}
+                            </p>
+                        )}
+
+                        {puesto && (
+                            <p style={{ color: '#FFFFFF', marginBottom: '1rem' }}>
+                                Puesto: {puesto}
+                            </p>
+                        )}
+                    </>
+                )}
+
+                {editable && (
+
+
+                    <>
+
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            onChange={handleChange}
+                            value={user.nombre}
+                        />
+
+                        <input
+                            type="email"
+                            id="correo"
+                            name="coreo"
+                            onChange={handleChange}
+                            value={user.correo}
+                        />
+
+                        <input
+                            type="text"
+                            id="puesto"
+                            name="puesto"
+                            onChange={handleChange}
+                            value={user.puesto}
+                        />
+
+
+                        <button
+                            type="submit"
+                            onClick={updateHandle}
+                            style={{ width: '35%', padding: '0.75rem', backgroundColor: '#000000', color: "#FFFFFF", border: 'none', borderRadius: '0.25rem', fontSize: '0.9rem', fontWeight: '500', cursor: 'pointer', marginTop: '1rem', ':hover': { backgroundColor: '#388E3C' } }}
+                        >
+                            Update
+                        </button>
+
+                    </>
+                ) }
+
                 
-                {name && (
-                    <h3 style={{ marginBottom: '0.5rem', fontSize: '1.25rem', fontWeight: '500', lineHeight: '1.2', color: "#FFFFFF" }}>
-                        {name}
-                    </h3>
-                )}
 
-                {mail && (
-                    <p style={{ color: '#FFFFFF', marginBottom: '1rem' }}>
-                        Mail: {mail}
-                    </p>
-                )}
-
-                {puesto && (
-                    <p style={{ color: '#FFFFFF', marginBottom: '1rem' }}>
-                        Puesto: {puesto}
-                    </p>
-                )}
-
+                
                 <button
                     type="submit"
-                    onClick={handleSubmit}
+                    onClick={toggleEdit}
                     style={{ width: '35%', padding: '0.75rem', backgroundColor: '#000000', color: "#FFFFFF", border: 'none', borderRadius: '0.25rem', fontSize: '0.9rem', fontWeight: '500', cursor: 'pointer', marginTop: '1rem', ':hover': { backgroundColor: '#388E3C' } }}
                 >
-                    Disable
+                    {editable ? "Cancel":"Edit"}
                 </button>
 
             </div>
--- src/firestore.js
+++ src/firestore.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, setDoc, doc, updateDoc} from "firebase/firestore";
+import { collection, getDocs, setDoc, doc, updateDoc, deleteDoc} from "firebase/firestore";
 import { db } from './firebase';
 import React, { useState } from "react";
 
@@ -19,12 +19,23 @@ export async function getData(collectionName) {
 
 export async function setData(collectionName, documentData) {
     try {
-        const documents = await getData(collectionName); 
-        const nextId = "ID: " + (documents.length + 1);
-        const docRef = doc(db, collectionName, nextId.toString());
-        await setDoc(docRef, {
-            ...documentData
+        const documents = await getData(collectionName);
+
+        
+        let maxId = 0;
+        documents.forEach(doc => {
+            const idParts = doc.id.split(': ');
+            if (idParts.length === 2 && !isNaN(idParts[1])) {
+                const currentId = parseInt(idParts[1]);
+                if (currentId > maxId) {
+                    maxId = currentId;
+                }
+            }
         });
+
+        const nextId = "ID: " + (maxId + 1); 
+        const docRef = doc(db, collectionName, nextId.toString());
+        await setDoc(docRef, { ...documentData });
     } catch (error) {
         console.error("Error uploading documents: ", error);
     }
@@ -32,16 +43,28 @@ export async function setData(collectionName, documentData) {
 
 export async function updateData(collectionName, documentData, id) {
     try {
-        console.log("Attempting to update:", {
-            collectionName,
-            id,
-            documentData
-        });
-
         const docRef = doc(db, collectionName, id);
         await updateDoc(docRef, documentData);
 
         console.log("Update successful");
+        return true;
+    } catch (error) {
+        console.error("Detailed error:", {
+            errorMessage: error.message,
+            errorCode: error.code,
+            fullError: error
+        });
+        return false;
+    }
+}
+
+
+export async function deleteData(collectionName, documentData, id) {
+    try {
+        const docRef = doc(db, collectionName, id);
+        await deleteDoc(docRef, documentData);
+
+        console.log("Deletion successful");
         return true;
     } catch (error) {
         console.error("Detailed error:", {
